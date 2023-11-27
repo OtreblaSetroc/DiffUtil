01fc63ae47801cc15691557f8ad8bdf9dc0482a4
Sergio Alberto Cortés González <sergioalbertocortesgonzalez@MacBook-Pro-de-Sergio.local>
2023-11-27 11:19:32 -0600

    add visibility to setCard

--- app/src/main/java/com/cogsa/diffutil/ui/adpter/viewholder/MyBodyViewHolder.kt
+++ app/src/main/java/com/cogsa/diffutil/ui/adpter/viewholder/MyBodyViewHolder.kt
@@ -4,18 +4,20 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.cogsa.diffutil.databinding.MyBodyBinding
 
-internal class MyBodyViewHolder(private val binding: MyBodyBinding) : RecyclerView.ViewHolder(binding.root),
+internal class MyBodyViewHolder(private val binding: MyBodyBinding) :
+    RecyclerView.ViewHolder(binding.root),
     MyBodyView {
     override fun setCard(cardType: String, cardNumber: String, money: String) {
-        with(binding){
-            cardNumberTextView.text =cardNumber
-            cardTypeTextView.text=cardType
-            cardValueTextView.text=money
+        with(binding) {
+            cardView.visibility = View.VISIBLE
+            cardNumberTextView.text = cardNumber
+            cardTypeTextView.text = cardType
+            cardValueTextView.text = money
         }
     }
 
     override fun hideCard() {
-      binding.cardView.visibility=View.GONE
+        binding.cardView.visibility = View.GONE
     }
 
     override fun setOnClickListener(type: String, func: ((type: String) -> Unit)?) {
